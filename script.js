c4e9e7e486ad972c31924690193f1d1907b703ce
Ronnie740 <ronnieineza@gmail.com>
2021-07-25 18:03:58 +0300

    commit

--- script.js
+++ script.js
@@ -16,7 +16,9 @@ menuIcon.addEventListener("click", () => {
 const learnMore = document.getElementById("l1");
 const overlay = document.getElementById("overlay");
 const title = document.getElementById("skillTitle");
+
 learnMore.addEventListener("click", skillInfo);
+
 function skillInfo() {
   overlay.classList.remove("hidden");
   title.classList.add("hidden");
@@ -28,7 +30,9 @@ function skillInfo() {
 const learnMore_2 = document.getElementById("l2");
 const overlay_2 = document.getElementById("overlay2");
 const title_2 = document.getElementById("skillTitle2");
+
 learnMore_2.addEventListener("click", skillInfo_2);
+
 function skillInfo_2() {
   overlay_2.classList.remove("hidden");
   title_2.classList.add("hidden");
@@ -40,7 +44,9 @@ function skillInfo_2() {
 const learnMore_3 = document.getElementById("l3");
 const overlay_3 = document.getElementById("overlay3");
 const title_3 = document.getElementById("skillTitle3");
+
 learnMore_3.addEventListener("click", skillInfo_3);
+
 function skillInfo_3() {
   overlay_3.classList.remove("hidden");
   title_3.classList.add("hidden");
@@ -52,7 +58,9 @@ function skillInfo_3() {
 const learnMore_4 = document.getElementById("l4");
 const overlay_4 = document.getElementById("overlay4");
 const title_4 = document.getElementById("skillTitle4");
+
 learnMore_4.addEventListener("click", skillInfo_4);
+
 function skillInfo_4() {
   overlay_4.classList.remove("hidden");
   title_4.classList.add("hidden");
@@ -64,7 +72,9 @@ function skillInfo_4() {
 const learnMore_5 = document.getElementById("l5");
 const overlay_5 = document.getElementById("overlay5");
 const title_5 = document.getElementById("skillTitle5");
+
 learnMore_5.addEventListener("click", skillInfo_5);
+
 function skillInfo_5() {
   overlay_5.classList.remove("hidden");
   title_5.classList.add("hidden");
@@ -76,7 +86,9 @@ function skillInfo_5() {
 const learnMore_6 = document.getElementById("l6");
 const overlay_6 = document.getElementById("overlay6");
 const title_6 = document.getElementById("skillTitle6");
+
 learnMore_6.addEventListener("click", skillInfo_6);
+
 function skillInfo_6() {
   overlay_6.classList.remove("hidden");
   title_6.classList.add("hidden");
